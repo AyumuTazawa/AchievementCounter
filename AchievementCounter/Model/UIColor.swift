abf99e60db56e32867ce4a4c2a5592beddd96db3
AyumuTazawa <tazawaayumu@gmail.com>
2020-12-06 01:11:46 +0900

    Initial Commit

--- AchievementCounter/Model/UIColor.swift
+++ AchievementCounter/Model/UIColor.swift
@@ -1,8 +1,8 @@
 //
-//   Color.swift
+//  UIColor.swift
 //  AchievementCounter
 //
-//  Created by 田澤歩 on 2020/12/05.
+//  Created by 田澤歩 on 2020/12/06.
 //  Copyright © 2020 net.ayumutazawa. All rights reserved.
 //
 
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
         return self.init(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
     }
 }
