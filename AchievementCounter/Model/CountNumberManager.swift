1fe912aa29d970d3146b96d164e6730ffca25b33
AyumuTazawa <tazawaayumu@gmail.com>
2020-12-07 18:44:24 +0900

    PromiseKitで修正

--- AchievementCounter/Model/CountNumberManager.swift
+++ AchievementCounter/Model/CountNumberManager.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import CoreData
+import PromiseKit
 
 protocol CountNumberManagerDelegate: class {
     func showCountNumber()
@@ -22,89 +23,122 @@ class CountNumberManager {
     static let appDelegate = UIApplication.shared.delegate as! AppDelegate
     let context = appDelegate.persistentContainer.viewContext
     
-    func plassNumber() {
-        if fecthCountNumber == nil {
-            var plassnumber = 0
-            plassnumber += 1
-            fecthCountNumber = plassnumber
-            delgate?.showCountNumber()
-        } else {
-            fecthCountNumber += 1
-            delgate?.showCountNumber()
+    func plassNumber() -> Promise<Int> {
+        return Promise { resolver in
+            if fecthCountNumber == nil {
+                       var plassnumber = 0
+                       plassnumber += 1
+                       fecthCountNumber = plassnumber
+                       delgate?.showCountNumber()
+                   } else {
+                       fecthCountNumber += 1
+                       delgate?.showCountNumber()
+                   }
+             resolver.fulfill(fecthCountNumber)
+            print("aa")
+            print(fecthCountNumber)
+            print("aa")
         }
+       
     }
     
-    func minusCount() {
-        if fecthCountNumber == nil {
-            var plassnumber = 0
-            plassnumber -= 1
-            fecthCountNumber = plassnumber
-            delgate?.showCountNumber()
-        } else {
-            fecthCountNumber -= 1
-            delgate?.showCountNumber()
-        }
-    }
-
-    
-    func saveData(with saveNumber: Int) {
-        let entity = NSEntityDescription.entity(forEntityName: "CountNumber", in: context)
-        let newCountedNumber = NSManagedObject(entity: entity!, insertInto: context)
-        //let saveData = saveNumber
-        newCountedNumber.setValue(saveNumber, forKey: "countNumber")
-        do{
-            try context.save()
-            print("save")
-        }catch {
-            print("err")
-        }
-    }
-    
-    func fecthData() {
-        let reqest = NSFetchRequest<NSFetchRequestResult>(entityName: "CountNumber")
-        reqest.returnsObjectsAsFaults = false
-        do{
-            let result = try context.fetch(reqest)
-            for data in result as! [NSManagedObject]{
-                let getNumber = data.value(forKey: "countNumber") as! Int
-                fecthCountNumber = getNumber
+    func minusCount() -> Promise<Void> {
+         return Promise { resolver in
+            if fecthCountNumber == nil {
+                var plassnumber = 0
+                plassnumber -= 1
+                fecthCountNumber = plassnumber
                 delgate?.showCountNumber()
+                print("nil")
+            } else {
+                fecthCountNumber -= 1
+                delgate?.showCountNumber()
+                print("else")
             }
-        }catch{
-            print("err")
+            resolver.fulfill(())
         }
     }
     
-    func deleteData() {
-        let reqest = NSFetchRequest<NSFetchRequestResult>(entityName: "CountNumber")
-        reqest.returnsObjectsAsFaults = false
-        do{
-            let result = try context.fetch(reqest)
-            print(result)
-            
-            for data in result as! [NSManagedObject]{
-                context.delete(data)
+    func saveData(with saveNumber: Int) -> Promise<Void> {
+        return Promise { resolver in
+            print("saveNumber")
+            print(saveNumber)
+            let entity = NSEntityDescription.entity(forEntityName: "CountNumber", in: context)
+            let newCountedNumber = NSManagedObject(entity: entity!, insertInto: context)
+            //let saveData = saveNumber
+            newCountedNumber.setValue(saveNumber, forKey: "countNumber")
+            do{
                 try context.save()
-                fecthCountNumber = 0
-                delgate?.showCountNumber()
+                
+            }catch {
+                print("err")
             }
-        }catch{
-            print("err")
+            resolver.fulfill(())
         }
     }
     
-    func updataData(with updataNumber: Int) {
-           let reqest = NSFetchRequest<NSFetchRequestResult>(entityName: "CountNumber")
-           reqest.returnsObjectsAsFaults = false
-           let myResults = try! context.fetch(reqest)
-           for data in myResults as! [NSManagedObject] {
-               data.setValue(updataNumber, forKey: "countNumber")
-           }
-           do{
-               try context.save()
-               print("アップデート")
-           }catch {
-               print("err")
-           }
-       }
+    func fecthData() -> Promise<Void> {
+         return Promise { resolver in
+            let reqest = NSFetchRequest<NSFetchRequestResult>(entityName: "CountNumber")
+                   reqest.returnsObjectsAsFaults = false
+                   do{
+                       let result = try context.fetch(reqest)
+                       for data in result as! [NSManagedObject]{
+                           let getNumber = data.value(forKey: "countNumber") as! Int
+                           fecthCountNumber = getNumber
+                           print("fecthData")
+                        print(getNumber)
+                           delgate?.showCountNumber()
+                        
+                       }
+                   }catch{
+                       print("err")
+                   }
+            resolver.fulfill(())
+        }
+    }
+    
+    func deleteData() -> Promise<Void> {
+        return Promise { resolver in
+            let reqest = NSFetchRequest<NSFetchRequestResult>(entityName: "CountNumber")
+            reqest.returnsObjectsAsFaults = false
+            do{
+                let result = try context.fetch(reqest)
+                print(result)
+                
+                for data in result as! [NSManagedObject]{
+                    context.delete(data)
+                    try context.save()
+                    fecthCountNumber = 0
+                    delgate?.showCountNumber()
+                   
+                }
+            }catch{
+                print("err")
+            }
+             resolver.fulfill(())
+        }
+    }
+    
+    
+     func updataData(with updataNumber: Int) -> Promise<Void> {
+        print("updataNumber")
+        print(updataNumber)
+        return Promise { resolver in
+            let reqest = NSFetchRequest<NSFetchRequestResult>(entityName: "CountNumber")
+            reqest.returnsObjectsAsFaults = false
+            let myResults = try! context.fetch(reqest)
+            for data in myResults as! [NSManagedObject] {
+                data.setValue(updataNumber, forKey: "countNumber")
+            }
+            do{
+                try context.save()
+                print("アップデート")
+            }catch {
+                print("err")
+            }
+            resolver.fulfill(())
+        }
+    }
+   
 }
