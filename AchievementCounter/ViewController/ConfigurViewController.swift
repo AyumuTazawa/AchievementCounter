db012ca0501df209f2d366074b71f460239a54b9
AyumuTazawa <tazawaayumu@gmail.com>
2020-12-11 15:50:14 +0900

    Initial Commit

---
+++ AchievementCounter/ViewController/ConfigurViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ConfigurViewController.swift
+//  AchievementCounter
+//
+//  Created by 田澤歩 on 2020/12/11.
+//  Copyright © 2020 net.ayumutazawa. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class ConfigurViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
