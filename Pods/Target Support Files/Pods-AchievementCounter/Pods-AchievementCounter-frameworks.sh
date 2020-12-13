5b3e00d92286f6b5bd1dbd4a5c4aac68ddf44d3e
AyumuTazawa <tazawaayumu@gmail.com>
2020-12-13 19:48:11 +0900

    Initial Commit

--- Pods/Target Support Files/Pods-AchievementCounter/Pods-AchievementCounter-frameworks.sh
+++ Pods/Target Support Files/Pods-AchievementCounter/Pods-AchievementCounter-frameworks.sh
@@ -175,9 +175,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftConfettiView/SwiftConfettiView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftConfettiView/SwiftConfettiView.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
