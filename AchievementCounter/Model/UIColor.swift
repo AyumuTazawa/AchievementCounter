fd96af6e0d4049c9eff92fe8d5b12e83e5c9f7d6
AyumuTazawa <tazawaayumu@gmail.com>
2020-12-17 00:41:01 +0900

    カラーコードに変更

--- AchievementCounter/Model/UIColor.swift
+++ AchievementCounter/Model/UIColor.swift
@@ -10,7 +10,11 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor{
-        return self.init(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
-    }
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+            let v = Int("000000" + hex, radix: 16) ?? 0
+            let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
+            let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
+            let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
+            self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+        }
 }
